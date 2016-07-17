9061477a8d98ac4cf01e95fd649a5190df377cfd
a_sitnikov <a_sitnikov@head-point.ru>
2016-07-17 14:41:47 +0300

    count inversions version beta

--- CountInversions/Program.cs
+++ CountInversions/Program.cs
@@ -8,7 +8,7 @@ namespace CountInversions
     {
         static void Main(string[] args)
         {
-            var paths = new[] { "mytest2" };
+            var paths = new[] { "mytest2.txt" };
 
             foreach (var path in paths)
             {
@@ -17,7 +17,8 @@ namespace CountInversions
                 {
                     var text = file.ReadToEnd();
                     var input = text.Split('\n').Select(int.Parse).ToArray();
-                    var inversionsCount = DoCountInversions(input, 0);
+                    int inversionsCount;
+                    SortAndCount(input, 0, out inversionsCount);
                     Console.WriteLine($"Inversions count = {inversionsCount}");
                 }
                 Console.WriteLine("-------------------");
@@ -25,29 +26,93 @@ namespace CountInversions
             }            
         }
 
-        private static int DoCountInversions(int[] input, int level)
+        private static int[] SortAndCount(int[] input, int level, out int initialCount)
         {
             Console.WriteLine($"level = {level}");
+            if (input.Length == 1)
+            {
+                initialCount = 0;
+                return input;
+            }
+
+            if (input.Length == 2)
+            {
+                if (input[0] > input[1])
+                {
+                    initialCount = 1;
+                    var swap = input[0];
+                    input[0] = input[1];
+                    input[1] = swap;
+                }
+                else
+                {
+                    initialCount = 0;
+                }
+                return input;
+            }
+
             var middlePosition = input.Length / 2;
 
             var left = new int[middlePosition];
             Array.Copy(input, 0, left, 0, left.Length);
             int leftInversionsCount;
-            var leftSorted = DoCountAndSort(left, out leftInversionsCount);
+            var leftSorted = SortAndCount(left, level + 1,  out leftInversionsCount);
 
             var right = new int[input.Length - left.Length];
             Array.Copy(input, middlePosition, right, 0, right.Length);
             int rightInversionsCount;
-            var rightSorted = DoCountAndSort(right, out rightInversionsCount);
+            var rightSorted = SortAndCount(right, level + 1, out rightInversionsCount);
+            int splitInversionsCount;
+            var fullSorted = CountSplitInversions(leftSorted, rightSorted, out splitInversionsCount);
 
-            var splitInversionsCount = CountSplitInversions(leftSorted, rightSorted);
-
-            return leftInversionsCount + rightInversionsCount + splitInversionsCount;
+            initialCount = (leftInversionsCount + rightInversionsCount + splitInversionsCount);
+            return fullSorted;
         }
 
-        private static int[] DoCountAndSort(int[] input, out int initialCount)
+        private static int[] CountSplitInversions(int[] leftSorted, int[] rightSorted, out int splitInversionsCount)
         {
-            
+            var totalLength = leftSorted.Length + rightSorted.Length;
+            var result = new int[totalLength];
+
+            var i = 0;
+            var j = 0;
+            splitInversionsCount = 0;
+
+            for (int index = 0; index < totalLength; index++)
+            {
+                if (j >= rightSorted.Length)
+                {
+                    result[index] = leftSorted[i];
+                    i++;
+                }
+                else if (i >= leftSorted.Length)
+                {
+                    result[index] = rightSorted[j];
+                    j++;
+                }
+                else
+                {
+                    if (leftSorted[i] < rightSorted[j])
+                    {
+                        result[index] = leftSorted[i];
+                        i++;
+                    }
+                    else if (leftSorted[i] > rightSorted[j])
+                    {
+                        result[index] = rightSorted[j];
+                        splitInversionsCount += (leftSorted.Length - i - 1);
+                        j++;
+                    }
+                    else
+                    {
+                        result[index] = rightSorted[j];
+                        splitInversionsCount += (leftSorted.Length - i - 1);
+                        j++;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
