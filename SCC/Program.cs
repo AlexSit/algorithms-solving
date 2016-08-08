12ccf31c75e51ed7ede609dcfceac0a5f6829f59
a_sitnikov <a_sitnikov@head-point.ru>
2016-08-08 23:14:54 +0300

    долгая версия. новый поток. словарь с финишами инвертировал ключ-значение

--- SCC/Program.cs
+++ SCC/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 namespace SCC
 {
@@ -12,17 +14,29 @@ namespace SCC
         private static int _currentLeaderNumber = 0;
         private static int[] _leaders;
 
+        private static List<NodeInfo> _convertedInput;
+        private static int _convertedInputMax;
+        
+        //private static NodeInfo[] _reversedInput;
         private static int[] _answer = new int[5]; 
 
         static void Main(string[] args)
         {
+            var stackSize = 10*000*000;
+            var thread = new Thread(Target, stackSize);
+            thread.Start();
+        }
+
+        private static void Target()
+        {
+            Console.WriteLine("START START START");            
             var paths = new List<string>
             {
-                "tc1.txt",
+                //"tc1.txt",
                 //"tc2.txt",
-                //"tc3.txt",
-                //"tc4.txt"
-                //"input.txt"               
+                "tc3.txt",
+                //"tc4.txt",
+                //"input.txt"
             };
 
             var testCases = new List<TestCase>();
@@ -58,6 +72,8 @@ namespace SCC
 
         private static void AssertTestCase(TestCase testCase)
         {
+            ResetInputData();
+
             Console.WriteLine($"test case: {testCase.FilePath}");
 
             int[] actualAnswer = null;
@@ -79,8 +95,8 @@ namespace SCC
                 throw new Exception("Couldn't find the answer!");
             }
 
-            Console.WriteLine($"Expected: {testCase.Answer}, Actual: {actualAnswer}");
-            if (testCase.Answer.SequenceEqual(actualAnswer) && testCase.Answer != null)
+            Console.WriteLine($"Expected: {string.Join(", ", testCase.Answer)}, Actual: {string.Join(", ", actualAnswer)}");
+            if (!testCase.Answer.SequenceEqual(actualAnswer) && testCase.Answer != null)
             {
                 throw new Exception($"Test case {testCase.FilePath} failed!");
             }
@@ -88,13 +104,24 @@ namespace SCC
             Console.WriteLine();
         }
 
+        private static void ResetInputData()
+        {
+            _t = 0;
+            _finishingTimes = new Dictionary<int, int>();
+            _currentLeaderNumber = 0;
+
+            _answer = new int[5];
+        }
+
         private static void Scc(List<Tuple<int, int>> input)
         {
-            var convertedInput = PrepareInput(input);
-            _leaders = new int[convertedInput.Count];
+            _convertedInput = PrepareInput(input);
+            _leaders = new int[_convertedInput.Count];
 
-            DfsForFinishingTimes(convertedInput);
-            DfsForScc(convertedInput);
+            DfsForFinishingTimes();
+            _convertedInput.ForEach(x=>x.Explored = false);
+
+            DfsForScc();
 
             CollectTop5Leaders();
         }
@@ -106,10 +133,10 @@ namespace SCC
             {
                 if (!leaders.ContainsKey(_leaders[i]))
                 {
-                    leaders[i + 1] = new List<int>();
+                    leaders[_leaders[i]] = new List<int>();
                 }
 
-                leaders[i + 1].Add(_leaders[i]);
+                leaders[_leaders[i]].Add(i+1);
             }
 
             var topLeaders = leaders.OrderByDescending(x => x.Value.Count).Take(5).ToList();
@@ -126,67 +153,140 @@ namespace SCC
             //{
             //    if(result.length)
             //}
-            return input.GroupBy(x => x.Item1, x=>x.Item2, (key, values) => new NodeInfo
+
+
+
+            var convertedInput = input.GroupBy(x => x.Item1, x=>x.Item2, (key, values) => new NodeInfo
             {
                 I = key,
                 Explored = false,
                 DestinationNodes = values.ToArray()
-            }).ToList();            
+            })
+            .ToList();
+
+            //var dstOnlyNodes = input
+            //    .Where(x => !convertedInput.Any(y => y.I == x.Item2))                
+            //    .Select(x => x.Item2)
+            //    .Distinct()
+            //    .Select(x => new NodeInfo
+            //    {
+            //        Explored = false,
+            //        I = x,
+            //        DestinationNodes = new int[0]
+            //    });
+
+            var count = convertedInput.Count;
+            for (int i = 0; i < count; i++)
+            {
+                if (convertedInput[i].I != i + 1)
+                {
+                    convertedInput.Insert(i, new NodeInfo
+                    {
+                        I = i+1,
+                        Explored = false,
+                        DestinationNodes = new int[0]
+                    });
+                    count++;
+                }
+            }
+
+            //_reversedInput = new NodeInfo[count];
+            //for (int i = 0; i < convertedInput.Count; i++)
+            //{
+            //    var destinationNodes = convertedInput[i].DestinationNodes;
+            //    for (int j = 0; j < destinationNodes.Length; j++)
+            //    {
+            //        var dstNode = destinationNodes[j];
+            //        if (_reversedInput[dstNode - 1] == null)
+            //        {
+            //            _reversedInput[dstNode - 1] = new NodeInfo
+            //            {
+            //                I = dstNode,
+            //                Explored = false
+            //            };
+            //            var cnt = convertedInput.Count(x => x.DestinationNodes.Contains(dstNode));  
+            //            _reversedInput[dstNode - 1].DestinationNodes = new int[cnt];
+            //        }
+
+
+            //        _reversedInput[dstNode - 1].DestinationNodes[_reversedInput[dstNode - 1].DestinationNodes.Length - 1] = 
+            //            convertedInput[i].I;
+            //    }
+            //}
+
+            _convertedInputMax = _convertedInput.Max(x => x.I);
+            return convertedInput.OrderBy(x=>x.I).ToList();            
         }
 
-        private static void DfsForFinishingTimes(List<NodeInfo> input)
+        private static void DfsForFinishingTimes()
         {
             _t = 0;
-            for (var i = input.Max(x=>x.I); i >= 1; i--)
+            for (var i = _convertedInputMax; i >= 1; i--)
             {
-                if (!input[i - 1].Explored)
+                if (!_convertedInput[i - 1].Explored)
                 {
-                    DfsReversed(input, i);
+                    DfsReversed(i);
                 }
             }
         }
 
-        private static void DfsReversed(List<NodeInfo> input, int i)
+        private static void DfsReversed(int i)
         {
-            var node = input[i - 1];
+            var node = _convertedInput[i - 1];
             node.Explored = true;
-            var destinationNodes = input.Where(x => x.DestinationNodes.Contains(i)).Select(x => x.I);
+            //NOTE
+            var destinationNodes = _convertedInput.Where(x => x.DestinationNodes.Contains(i)); //            var destinationNodes = _convertedInput[i-1].DestinationNodes;
             foreach (var dstNodeIndex in destinationNodes)
             {
-                var dstNode = input[dstNodeIndex - 1];
+                var dstNode = _convertedInput[dstNodeIndex.I - 1];
                 if (!dstNode.Explored)
                 {
-                    DfsReversed(input, dstNodeIndex);
+                    DfsReversed(dstNodeIndex.I);
                 }
             }
             _t++;            
-            _finishingTimes[i - 1] = _t;
+            _finishingTimes[_t] = i;
+            if (_t < 1000 && _t % 100 == 0)
+            {
+                Console.WriteLine(_t);
+            }
+            else if (_t < 10000 && _t%1000 == 0)
+            {
+                Console.WriteLine(_t);
+            }
+            else if (_t < 100000 && _t % 10000 == 0)
+            {
+                Console.WriteLine(_t);
+            }
         }
 
-        private static void Dfs(List<NodeInfo> input, int i)
+        private static void Dfs(int i)
         {
-            var node = input[i - 1];
+            var node = _convertedInput[i - 1];
             node.Explored = true;
             _leaders[i - 1] = _currentLeaderNumber;
             foreach (var dstNodeIndex in node.DestinationNodes)
             {
-                var dstNode = input[dstNodeIndex - 1];
+                var dstNode = _convertedInput[dstNodeIndex - 1];
                 if (!dstNode.Explored)
                 {
-                    Dfs(input, dstNodeIndex);
+                    Dfs(dstNodeIndex);
                 }
             }            
         }
 
-        private static void DfsForScc(List<NodeInfo> input)
+        private static void DfsForScc()
         {
             _currentLeaderNumber = 0;
-            for (var i = input.Max(x => x.I); i >= 1; i--)
+            var maxFinishingTime = _convertedInputMax; //_finishingTimes.Keys.Max();
+            for (var finishingTime = maxFinishingTime; finishingTime >= 1; finishingTime--)
             {
-                if (!input[i - 1].Explored)
+                //NOTE - можно сделать финиш. время ключом
+                var i = _finishingTimes[finishingTime];
+                if (!_convertedInput[i - 1].Explored)
                 {
-                    _currentLeaderNumber = i;
-                    Dfs(input, i);
+                    _currentLeaderNumber = finishingTime;
+                    Dfs(i);
                 }
             }
         }
