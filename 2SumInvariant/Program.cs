d41ef1d4f55f4d3c483dda964885e93754f32e51
a_sitnikov <a_sitnikov@head-point.ru>
2016-08-22 12:30:00 +0100

    Naive implementation of 2-sum problem.

---
+++ 2SumInvariant/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _2SumInvariant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("START START START");
+            var basePath = "../../inputs";
+            var paths = new List<string>
+            {                
+                //"tc1.txt",
+                //"tc2.txt",
+                "2sum.txt"
+            };
+
+            foreach (var path in paths)
+            {
+                using (var file = File.OpenText(Path.Combine(basePath, path)))
+                {
+                    var text = file.ReadToEnd();
+                    var input = ProcessTestCaseText(text);
+
+                    var answer = GetAnswer(input);
+
+                    Console.WriteLine($"Actual ANSWER: {answer}");
+                }
+            }
+            Console.WriteLine("the end");
+            Console.ReadKey();
+        }
+
+        private static long GetAnswer(Hashtable input)
+        {
+            List<Tuple<long, long>> answer = new List<Tuple<long, long>>();
+            var sums = new Hashtable();
+            for (long t = -10000; t <= 10000; t++)
+            {
+                if (!sums.ContainsKey(t))
+                {
+                    foreach (var key in input.Keys)
+                    {
+                        var x = (long) key;
+                        var y = t - x;
+                        if (x != y)
+                        {
+                            if (input.ContainsKey(y))
+                            {
+                                answer.Add(new Tuple<long, long>(x, y));
+                                sums.Add(t, true);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }                                
+
+            return answer.Count;
+        }
+
+        private static Hashtable ProcessTestCaseText(string text)
+        {
+            var input = new Hashtable();
+            var lines = text.Split('\n').Where(x => !string.IsNullOrWhiteSpace(x));
+            foreach (var line in lines)
+            {
+                var key = long.Parse(line);
+                if (!input.ContainsKey(key))
+                {
+                    input.Add(key, true);
+                }
+                else
+                {
+                    input.Remove(key);
+                }
+            }
+
+            return input;
+        }    
+    }
+}
