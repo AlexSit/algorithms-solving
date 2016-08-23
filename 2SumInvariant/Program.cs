a1c04a8947cbb141aaba1d450bcc90c0b513861a
a_sitnikov <a_sitnikov@head-point.ru>
2016-08-23 14:26:16 +0100

    Удалил лишнее. Небольшая оптимизация. up to 37 min

--- 2SumInvariant/Program.cs
+++ 2SumInvariant/Program.cs
@@ -34,73 +34,106 @@ namespace _2SumInvariant
             Console.WriteLine("the end");
             Console.ReadKey();
         }
-
-        private static long GetAnswer(Hashtable input)
-        {
-            List<Tuple<long, long>> answer = new List<Tuple<long, long>>();
-            var sums = new Hashtable();
-            for (long t = -10000; t <= 10000; t++)
-            {
-                if (!sums.ContainsKey(t))
-                {
-                    foreach (var key in input.Keys)
-                    {
-                        var x = (long) key;
-                        var y = t - x;
-                        if (x != y)
-                        {
-                            if (input.ContainsKey(y))
-                            {
-                                answer.Add(new Tuple<long, long>(x, y));
-                                sums.Add(t, true);
-                                break;
-                            }
-                        }
-                    }
-                }
-            }                                
-
-            return answer.Count;
-        }
-
+        
         private static long ReadInputAndSolve(string text)
         {
-            var sums = new Dictionary<long,long>();
-            for (long i = -10000; i <= 10000; i++)
+            var sums = new List<long>();
+
+            const int minT = -10000;
+            const int maxT = 10000;
+
+            for (long i = minT; i <= maxT; i++)
             {
-                sums.Add(i, i);
+                sums.Add(i);
             }
 
             long currentSumCount = 0;
 
-            var input = new Hashtable();
-            var lines = text.Split('\n').Where(x => !string.IsNullOrWhiteSpace(x)).Select(long.Parse).Distinct().ToList();
-            var linesCount = lines.Count;
-            var min = lines.Min();
-            var max = lines.Max();
-            for(var i = 0; i < linesCount; i++)
-            {                
-                input.Add(i, true);                
+            var inputHashtable = new Hashtable();
+            var inputList = text
+                .Split('\n')
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Select(long.Parse)
+                .Distinct()
+                .ToList();
+            inputList.Sort();
+            
+            var min = inputList.Min();
+            var max = inputList.Max();
+            long? maxLimit;
+            long? minLimit;
+            DefineLimits(min, max, maxT, minT, out maxLimit, out minLimit);
 
-                currentSumCount += CheckSumCriteria(input, i, ref sums);                
+            if (minLimit.HasValue || maxLimit.HasValue)
+            {
+                inputList = TruncateInputList(inputList, minLimit, maxLimit);
+            }                
+
+            var inputListCount = inputList.Count;
+            for (var inputIndex = 0; inputIndex < inputListCount; inputIndex++)
+            {
+                var inputItem = inputList[inputIndex];
+                inputHashtable.Add(inputItem, true);                
+
+                currentSumCount += CheckSumCriteria(inputHashtable, inputItem, ref sums);                
             }
 
             return currentSumCount;
         }
 
-        private static long CheckSumCriteria(Hashtable input, long x, ref Dictionary<long, long> sums)
+        private static List<long> TruncateInputList(List<long> inputList, long? minLimit, long? maxLimit)
         {
-            long foundSumCount = 0;
-            var pairs = sums.ToList();
-            var count = pairs.Count;
-            for (int i = 0; i < count; i++)
+            if (minLimit.HasValue)
+            {
+                return inputList.Where(x => x >= minLimit.Value).ToList();
+            }
+            else if (maxLimit.HasValue)
+            {
+                return inputList.Where(x => x <= maxLimit.Value).ToList();
+            }
+
+            return inputList;
+        }
+
+        /// <summary>
+        /// Если возможно, сокращает границы входного массива на основании того, что не все элементы дадут сумму в нужных границах.
+        /// </summary>
+        /// <param name="min">Минимум во входном массиве</param>
+        /// <param name="max">Максимум во входном массиве</param>
+        /// <param name="maxT">Максимально возможная сумма (верхняя граница)</param>
+        /// <param name="minT">Минимально возможная сумма (нижняя граница)</param>
+        /// <param name="maxLimit">Результат: максимальное значение во входном массиве, дальше которого искать элементы суммы бесполезно</param>
+        /// <param name="minLimit">Результат: минимальное значение во входном массиве, раньше которого искать элементы суммы бесполезно</param>
+        private static void DefineLimits(long min, long max, int maxT, int minT, out long? maxLimit, out long? minLimit)
+        {            
+            maxLimit = null;
+            minLimit = null;
+            
+            if (min + max > maxT)
+            {
+                maxLimit = maxT - min;
+            }
+            else if (min + max < minT)
+            {
+                minLimit = minT - max;
+            }
+        }
+
+        private static long CheckSumCriteria(Hashtable input, long inputItem, ref List<long> sums)
+        {
+            long foundSumCount = 0;            
+            var count = sums.Count;
+            for (int sumIndex = 0; sumIndex < count; sumIndex++)
             {                                
-                var t = sums[pairs[i].Key];
-                var y = (t - x);
-                if (input.ContainsKey(y))
+                var t = sums[sumIndex];
+
+                var y = (t - inputItem);                
+                if (inputItem != y && input[y] != null)
                 {
+                    //Console.WriteLine($"{inputItem} + {y} = {t}");
                     sums.Remove(t);
                     count--;
+                    sumIndex--;
                     foundSumCount++;
                 }                
             }
