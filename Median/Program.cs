a159f72f7613d5dcf748a42aa226700d58ea564d
a_sitnikov <a_sitnikov@head-point.ru>
2016-08-23 10:21:02 +0100

    Created basic of solution

--- Median/Program.cs
+++ Median/Program.cs
@@ -1,9 +1,70 @@
-﻿namespace Median
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Median
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("START START START");
+            var basePath = "../../inputs";
+            var paths = new List<string>
+            {                
+                "tc1.txt",                
+                //"median.txt"
+            };
+
+            foreach (var path in paths)
+            {
+                using (var file = File.OpenText(Path.Combine(basePath, path)))
+                {
+                    var text = file.ReadToEnd();
+                    var answer = ReadInputAndSolve(text);
+
+                    //var answer = GetAnswer(input);
+
+                    Console.WriteLine($"Actual ANSWER: {answer}");
+                }
+            }
+            Console.WriteLine("the end");
+            Console.ReadKey();
         }
+
+        private static int ReadInputAndSolve(string text)
+        {
+            var lines = text.Split('\n').Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+            var linesCount = lines.Count;
+
+            var m = new int[linesCount];
+            var heap = new Heap();
+            for (var i = 0; i < linesCount; i++)
+            {
+                var number = int.Parse(lines[i]);
+                AddToHeap(heap, number);
+                var mk = GetMedian(heap);
+                m[i] = mk;
+            }
+
+            var medianSum = m.Aggregate(0, (i, i1) => i + i1);
+            var answer = medianSum%10000;
+            return answer;
+        }
+
+        private static int GetMedian(Heap heap)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AddToHeap(Heap heap, int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Heap
+    {
     }
 }
