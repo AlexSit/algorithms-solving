fc9ce105a1cb3e5d21a6e99bb1df086480df3d87
a_sitnikov <a_sitnikov@head-point.ru>
2016-10-02 19:35:46 +0300

    clustering_big, test cases passed, but main case didn't

--- clustering/clustering_big.py
+++ clustering/clustering_big.py
@@ -1,10 +1,55 @@
+import sys
+
 from union_find import *
 
-def put_neighbours_to_same_cluster(clusters, node, shortest_distance):
+def invert_bit_in_number(number, bit_index):
+    bit = (node & 2**bit_index) / 2**bit_index
+    if bit == 1:            
+        return (number - 2**bit_index)
+    else:            
+        return (number + 2**bit_index)
 
-    pass
+def process_neighbour(clusters, neighbour, uf, node):
+    if neighbour in clusters:                    
+        #если есть, то ищем в объединении
+        cl_neighbour = uf.find(neighbour)
+        cl_ethalon = uf.find(node)            
+        #теперь возможны два случая
+        # он в том же кластере, что и эталон, то оставляем всё как есть
+        # он в другом кластере
+        if cl_neighbour.parent != cl_ethalon.parent:
+            # если эталон сам по себе, то эталон включаем в соседский кластер
+            # если эталон уже в кластере, а сосед в другом, то включаем меньший кластер в больший
+            # т.е. в любом случае включаем меньший кластерв больший, этим занимается реализация UnionFind
+            uf.union(cl_neighbour.parent, cl_ethalon.parent)
+def is_spacing_exceeded(cnt_bits, x, y, max_distance):
+    mask = 1
+    differenceCount = 0
+    i = 0
+    while i < cnt_bits:    
+         a = x & mask
+         b = y & mask
+         
+         if a != b:             
+             differenceCount += 1
 
-input = open('inputs/tc1_big.txt')
+         mask = mask << 1
+         i += 1 
+    
+    return differenceCount > max_distance
+
+print(sys.version)
+
+#input = open('inputs/my1_big.txt') #4
+#input = open('inputs/my2_big.txt') #2
+#input = open('inputs/my3_big.txt') #1
+#input = open('inputs/my4_big.txt') #8
+#input = open('inputs/tc1_big.txt') #7
+#input = open('inputs/tc2_big.txt') #1
+input = open('inputs/tc3_big.txt') #4
+#input = open('inputs/tc4_big.txt') #1
+#input = open('inputs/tc5_big.txt') #6
+#input = open("inputs/clustering_big.txt")
 lines = input.read().splitlines()
 first_line_def = lines[0].split()
 
@@ -14,43 +59,70 @@ print("cnt_nodes = %d" % cnt_nodes)
 cnt_bits = int(first_line_def[1])
 print("cnt_bits = %d" % cnt_bits)
 
-clusters = dict.fromkeys([int(line.replace(' ', ''), 2) for line in lines[1:]])
+clusters = {}
+for index, line in enumerate(lines[1:]):    
+    clusters[int(line.replace(' ', ''), 2)] = index
+    #print(clusters)
+
 uf = UnionFind(clusters)
 
 print('hamming distances have been read')
 
-shortest_distance = 2
+spacing = 2
 for node in clusters:
+    #print(bin(node)[2:])
     # вычислить соседа с помощью каждого из cnt_bits битов    
-    bit_index = 0    
-    while bit_index < cnt_bits:
-        i_position_bit = (node & 2**bit_index) / 2**bit_index
-        neighbour = None
-        if i_position_bit == 1:            
-            neighbour = (node + 2**bit_index)
-        else:            
-            neighbour = (node + 2**bit_index)
-        #есть сосед (расстояние от эталона не больше shortest_distance)
-        #проверить, есть ли в массиве сосед
-        if neighbour in clusters:
-            #если есть, то ищем в объединении
-            сl_neighbour = uf.find(neighbour)
-            cl_ethalon = uf.find(node)            
-            #теперь возможны два случая
-            # он в том же кластере, что и эталон, то оставляем всё как есть
-            # он в другом кластере
-            if сl_neighbour != cl_ethalon:
-                # если эталон сам по себе, то эталон включаем в соседский кластер
-                # если эталон уже в кластере, а сосед в другом, то включаем меньший кластер в больший
-                # т.е. в любом случае включаем меньший кластерв больший, этим занимается реализация UnionFind
-                uf.union(cl_neighbour, cl_ethalon)
-        # будем переходить к следующему соседу в следующей итерации    
-        bit_index+=1
+    first_bit_index = 0        
+    while first_bit_index < cnt_bits:
+        neighbour_one_bit_inverted = invert_bit_in_number(node, first_bit_index) 
 
-cnt_clusters = len(clusters)
-print('shortest_distance = %d and cnt_clusters = %d' %  (shortest_distance, cnt_clusters))
+        #print('FIRST BIT INDEX %d' % first_bit_index)
+        #print("neighbour: %s" % bin(neighbour_one_bit_inverted)[2:])                  
+        process_neighbour(clusters, neighbour_one_bit_inverted, uf, node)
+
+        second_bit_index = first_bit_index + 1
+        while second_bit_index < cnt_bits:
+            if second_bit_index != first_bit_index:
+                neighbour_two_bits_inverted = invert_bit_in_number(neighbour_one_bit_inverted, second_bit_index)            
+        
+                #есть сосед (расстояние от эталона не больше spacing)
+                #проверить, есть ли в массиве сосед
+                #print("neighbour: %s" % bin(neighbour_two_bits_inverted)[2:])                  
+        
+                process_neighbour(clusters, neighbour_two_bits_inverted, uf, node)
+                second_bit_index+=1
+        # будем переходить к следующему соседу в следующей итерации    
+        first_bit_index+=1
+
+#define cluster count
+cluster_parents = {}
+for el in uf:  
+    if el.parent not in cluster_parents:
+        cluster_parents[el.parent] = []
+    cluster_parents[el.parent].append(el)    
+
+print('spacing = %d and cnt_clusters = %d' %  (spacing, len(cluster_parents)))
 print('finish')
 
+if True:
+    print('CHECK CORRECTNESS')    
+    found_distance_exceeded = False
+
+    uf_list = list(uf.elements.values())
+    cnt = len(uf_list)
+    el1_index = 0
+    while el1_index < cnt - 1:
+        el1 = uf_list[el1_index]
+        el2_index = el1_index + 1
+        while el2_index < cnt:
+            el2 = uf_list[el2_index]
+            if el1.parent != el2.parent and (not is_spacing_exceeded(cnt_bits, el1.number, el2.number, spacing)):
+                print('NUMBERS WITH DISTANCE <= 2 IN DIFFERENT CLUSTERS')
+
+            el2_index += 1
+        el1_index += 1
+
+    print('Done')
 '''
 1) Можно проверять, есть ли в массиве сосед с помощью хэш-таблицы
 2) И если есть, то ищем в объединении
--- clustering/union_find.py
+++ clustering/union_find.py
@@ -1,61 +1,71 @@
-__author__ = 'davide' # https://gist.github.com/DavideCanton/9173142 + my modifications
+__author__ = 'davide' # https://gist.github.com/DavideCanton/9173142 + @AlexSit (@asit) modifications 
 
 import collections
 
 class Element:
-    def __init__(self, parent, rank=0, size=1):
-        self.parent = parent
-        self.rank = rank
+    def __init__(self, number, size=1):        #rank = 0
+        self.number = number
+        self.parent = number
+        #self.rank = rank
         self.size = size
-
+    def __str__(self):
+        return 'number = %s, parent = %d, size = %d\n' % ("{0:b}".format(self.number), self.parent, self.size) # rank = %d,
 
 class UnionFind:
-    def __init__(self, list):
-        self.el = [Element(i) for i in list]
-        self.size = len(list)
+    def __init__(self, init_list):
+        self.elements = {}
+        for i in init_list:
+            self.elements[i] = Element(i)
+        self.size = len(init_list)
+
+    def printOneItemClusters(self):
+        result = ''      
+        for i in self.elements:            
+            if i == self.elements[i].parent:
+                print("number: " + str(i) + "; " +  str(self.elements[i]) + '; ')
 
     def find(self, x):
-        print('FIND')
+        #print('FIND: %d' % x)  
         cur = x
-        while cur != self.el[cur].parent:
-            cur = self.el[cur].parent
-        self.el[x].parent = cur
-        return cur
+        while cur != self.elements[cur].parent:
+            cur = self.elements[cur].parent
+        self.elements[x].parent = cur #заодно проставляем родителя
+        return self.elements[x]
 
-    def union(self, x, y):
-        if self.el[x].parent != x:
-            x = self.find(x)
-        if self.el[y].parent != y:
-            y = self.find(y)
-        if self.el[x].rank > self.el[y].rank:
-            self.el[y].parent = x
-            self.el[x].size += self.el[y].size
+    def union(self, parent1, parent2):
+        #print('UNION')
+        #print(parent1, parent2, sep=" ")
+        if self.elements[parent1].size > self.elements[parent2].size:
+            self.elements[parent2].parent = parent1            
+            self.elements[parent1].size += self.elements[parent2].size
         else:
-            self.el[x].parent = y
-            self.el[y].size += self.el[x].size
-            if self.el[x].rank == self.el[y].rank:
-                self.el[y].rank += 1
+            self.elements[parent1].parent = parent2
+            self.elements[parent2].size += self.elements[parent1].size
+            #if self.elements[parent1].rank == self.elements[parent2].rank:
+            #    self.elements[parent2].rank += 1
         self.size -= 1
-
-
+        #print('ITOG')
+        #print(self)
     def __len__(self):
         return self.size
 
     def size(self, x):
-        return self.el[x].size
+        return self.elements[x].size
 
     def __iter__(self):
-        for i, el in enumerate(self.el):
-            if el.parent == i:
-                yield i
+        for i, key in enumerate(self.elements):            
+            yield self.elements[key]
 
-    def __str__(self):
-        return " ".join(map(str, self))
+    def __str__(self):  
+        result = ''      
+        for i in self.elements:
+            result += "number: " + str(i) + "; " +  str(self.elements[i]) + '; '
+        return result
 
-if __name__ == "__main__":
-    u = UnionFind([1,2,3,4,5])
+if __name__ == "__main__":    
+    u = UnionFind([10,11,12,13,14]) 
     print(u)
-    u.union(0, 3)
+    u.union(10, 13)
     print(u)
-    print(u.find(0))
-    print(u.find(3))
+    print(u.find(10))
+    print(u.find(13))
