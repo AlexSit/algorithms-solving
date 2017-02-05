44704c6b4c9c1e68d9f01f1f73c32aa386707832
a_sitnikov <a_sitnikov@head-point.ru>
2017-02-05 20:17:08 +0300

    Number of input clauses decreased by repeating reductions of clauses where at least oen variable occurs only with one type of sign.

--- 2-sat/2sat.py
+++ 2-sat/2sat.py
@@ -13,10 +13,10 @@ global var_in_clauses
 global clauses_eval
 global unsat_cnt
 
-def get_var_to_flip(unsat_clause_positions):
+def get_var_to_flip(unsat_cnt, unsat_clause_positions):
     global clauses
 
-    r1 = random.choice(list(unsat_clause_positions.keys()))
+    r1 = random.randint(0, unsat_cnt-1)
     r2 = random.randint(0, 1)
     return abs(clauses[unsat_clause_positions[r1]][r2])
 
@@ -54,16 +54,12 @@ def check_clauses_from_scratch(clauses_cnt):
     clauses_eval = []
     iterations_number += 1
     try:
-        unsat_clause_positions = {}
-        unsat_clause_positions_cnt = 0        
+        unsat_clause_positions = []
         for c_index in range(clauses_cnt):
             isSat = eval_clause(c_index)            
             clauses_eval.append(isSat)            
             if not isSat:
-                unsat_clause_positions[c_index] = True
-                unsat_clause_positions_cnt += 1
-                #if unsat_clause_positions_cnt == 10: # NOTE let's only collect limited number of unsatisfying clauses 
-                #    break; 
+                unsat_clause_positions.append(c_index)
 
         return unsat_clause_positions
     except:        
@@ -86,18 +82,19 @@ def papadimitrous_algorithm(var_count):
     global clauses_eval
     global unsat_cnt
 
+    if not var_count:
+        print("succeeded: True (var_count == 0)")
+        return
+
     clauses_eval = []
     succeeded = False
     iter_count = round(math.log(var_count, 2))    
-    local_search_iter_count = 2*(var_count**2)    # 2*(var_count**2)   
+    local_search_iter_count = 2*(var_count**2)   
     clauses_cnt = len(clauses)   
 
     print("iter_count: {}".format(iter_count))
     print("local_search_iter_count: {}".format(local_search_iter_count))
 
-    #graphviz = GraphvizOutput()
-    #graphviz.output_file = './output.png'
-    #with PyCallGraph(output=GraphvizOutput()):    
     for attempt in range(iter_count):
         print("attempt: {}".format(attempt))
         print(datetime.datetime.now())
@@ -110,33 +107,64 @@ def papadimitrous_algorithm(var_count):
         # check if initial assignment is satisfying                       
         unsat_clause_positions = check_clauses_from_scratch(clauses_cnt) 
         unsat_cnt = len(unsat_clause_positions)                
-
         for local_search_index in range(local_search_iter_count):
             if local_search_index % 10000 == 0:
                 print("local_search_index: {}".format(local_search_index))
-                print(datetime.datetime.now())                
+                print(datetime.datetime.now())
             if not unsat_cnt:
                 succeeded = True                    
                 break;                
             #graphviz = GraphvizOutput()
             #graphviz.output_file = './output.png'
             #with PyCallGraph(output=GraphvizOutput()):    
-            flipped_var = get_var_to_flip(unsat_clause_positions)                
-            assignment[flipped_var] = not assignment[flipped_var]
+            flipped_var = get_var_to_flip(unsat_cnt, unsat_clause_positions) 
+            assignment[flipped_var] = not assignment[flipped_var]            
             for clause_to_check_pos in var_in_clauses[flipped_var]:
                 isSat_old = clauses_eval[clause_to_check_pos]
                 isSat = eval_clause(clause_to_check_pos)
                 clauses_eval[clause_to_check_pos] = isSat  
                 if isSat:
-                    unsat_clause_positions.pop(clause_to_check_pos, None)
+                    if clause_to_check_pos in unsat_clause_positions: unsat_clause_positions.remove(clause_to_check_pos)
                 else:        
-                    unsat_clause_positions[clause_to_check_pos] = True
-                unsat_cnt += (isSat_old - isSat)    
-                #sys.exit()                
+                    unsat_clause_positions.append(clause_to_check_pos)                            
+                unsat_cnt += (isSat_old - isSat)                    
    
     print("succeeded: {}".format(succeeded))
     print("iterations_number: {}".format(iterations_number))
 
+def reduce_clauses(input_clauses):    
+    global assignment    
+    global var_in_clauses
+
+    # read clauses from the input and remember whether variable occurs only positive or only negative but not both signs
+    vars_sign_info = {}
+    clauses = []
+    result_clauses = []
+    var_in_clauses = {}
+
+    for c in input_clauses:        
+        for v in c:
+            save_sign_info(vars_sign_info, v)
+        clauses.append(c)    
+        
+    clause_index = 0
+    for c in clauses:              
+        remove = False        
+        for v in c:
+            v = abs(v)
+            # if in couple at least one variable has only one sign then remove a couple
+            if vars_sign_info.get(v, -1) != 3: # variable has both signs; 1 +; 2 -; 0 - no info           
+                remove = True
+        if not remove:
+            for t in c:
+                if abs(t) not in assignment:
+                    assignment[abs(t)] = True # write down this variable and it won't be used ever later                                        
+                if abs(t) not in var_in_clauses: var_in_clauses[abs(t)] = [] # remember in which clauses variable exists
+                var_in_clauses[abs(t)].append(clause_index)
+            result_clauses.append(c)
+            clause_index += 1
+
+    return result_clauses
 
 def main():
     global clauses
@@ -149,41 +177,32 @@ def main():
     assignment = {}
     var_in_clauses = {}
 
-    f = open('./inputs/2sat1.txt')
+    '''
+    2sat1.txt = 1
+    2sat2.txt = 0
+    2sat3.txt = 1
+    2sat4.txt = 1
+    2sat5.txt = 0
+    2sat6.txt = 0
+    '''
+
+    f = open('./inputs/2sat5.txt')
     lines = f.read().splitlines()
-    var_count = int(lines[0])
+    raw_clauses = []   
+    for i in lines[1:]: raw_clauses.append( tuple( map(int, i.split()) ) )
+    print('reduce')
 
-    # read clauses from the input and remember whether variable occurs only positive or only negative but not both signs
-    vars_sign_info = {}
-    raw_clauses = []
-    for i in lines[1:]:
-        c = tuple( map(int, i.split()) )
-        for v in c:
-            save_sign_info(vars_sign_info, v)
-        raw_clauses.append(c)    
-
-    print('reduce')    
-    var_count = 0    
-    clause_index = 0
-    for c in raw_clauses:              
-        remove = False        
-        for v in c:
-            v = abs(v)
-            # if in couple at least one variable has only one sign then remove a couple
-            if vars_sign_info.get(v, -1) != 3: # variable has both signs; 1 +; 2 -; 0 - no info           
-                remove = True
-        if not remove:
-            for t in c:
-                if abs(t) not in assignment:
-                    assignment[abs(t)] = True # write down this variable and it won't be used ever later                    
-                    var_count += 1
-                if abs(t) not in var_in_clauses: var_in_clauses[abs(t)] = [] # remember in which clauses variable exists
-                var_in_clauses[abs(t)].append(clause_index)
-            clauses.append(c)
-            clause_index += 1
-
-    print('start')    
-    print(var_count)
+    num_reduction = len(raw_clauses)
+    print("num_reduction: {}".format(num_reduction))
+    for i in range(num_reduction):
+        if not i % 10000: print(i)
+        clauses = reduce_clauses(raw_clauses)
+        raw_clauses = clauses
+        
+    print('start')        
+    var_count = len(clauses)
+    print('var_count after reduction:')    
+    print(var_count)    
     papadimitrous_algorithm(var_count)
 
 if __name__ == '__main__':    
