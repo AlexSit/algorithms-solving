232d3837b05c766bea9c6ad24453aa3156a6e806
a_sitnikov <a_sitnikov@head-point.ru>
2017-06-28 22:45:27 +0100

    TSP working version, not optimised

--- travelling-salesman-problem/test_helper.py
+++ travelling-salesman-problem/test_helper.py
@@ -2,33 +2,33 @@ import math
 inf = float('inf')
 
 class TestCase:
-    def __init__(self, path_to_case, points_count):
-        self.path_to_case = path_to_case
+    def __init__(self, path, pointsCount):
+        self.path = path
         self.points = []
         self.answer = None
     def __str__(self):
         return str(self.points)  
-    def is_passed(self, actual_answer):
+    def isPassed(self, actual_answer):
         return self.answer == actual_answer
-    def parse_answer(self, line):
+    def _parseAnswer(self, line):
         self.answer = float(line.split(':')[1].strip())
-    def set_point(self, line):        
+    def setPoint(self, line):        
         (x, y) = tuple(map(float, line.split()))
         self.points.append((x, y))
 
-def read_cases(paths_to_cases):
+def readCases(paths):
     cases = []
-    for path_to_case in paths_to_cases:    
+    for path in paths:    
         # ------------- Считывание тестового файла --------
-        lines = open(path_to_case).read().splitlines()
-        (points_count, edge_count) = tuple(map(int, lines[0].split())) # !!!!!!!! NOTE может ли быть несколько параллельных рёбер     
-        case = TestCase(path_to_case, points_count)
+        lines = open(path).read().splitlines()
+        pointsCount = int(lines[0]) # !!!!!!!! NOTE может ли быть несколько параллельных рёбер     
+        case = TestCase(path, pointsCount)
         for line in lines[1:]:
-            if (line.strip() == ''):
+            if line.strip() == '':
                 continue
-            if(line.find('Answer') != -1):
-                case.parse_answer(line)
+            if line.find('Answer') != -1:
+                case._parseAnswer(line)
                 break
-            case.set_point(line)    
+            case.setPoint(line)    
         cases.append(case)
     return cases
--- travelling-salesman-problem/tsp.py
+++ travelling-salesman-problem/tsp.py
@@ -2,63 +2,100 @@ import sys
 import math
 import test_helper
 
+# from set_indexed_by_list import SetIndexedByList
+from set_indexed_by_collection import SetIndexedByTuple
+from collection_generator import generateTuplesWithoutReturnInAscending
+
+from pycallgraph import PyCallGraph
+from pycallgraph.output import GraphvizOutput
+
+global IntermediatePointsToDestination
+global C
+
+def calcDistance(x1, y1, x2, y2): return math.sqrt(math.pow(x1-x2, 2) + math.pow(y1-y2, 2))
+
+def calcDistances(points):    
+    length = len(points)
+    C = [[0 for j in range(length)] for i in range(length)]
+
+    for i in range(length):
+        for j in range(i+1, length):
+            dist = calcDistance(points[i][0], points[i][1], points[j][0], points[j][1])
+            C[i][j] = dist
+            C[j][i] = dist
+    return C 
+
 inf = float('inf')
 
-def tsp(points):
-    init BaseCase
 
-    n = len(points)
-    for m in range(2, n+1):
-        for each S:
-            for each j in S:
-                if j != 1:
-                    min_result = inf
-                    for k in S:
-                        if k != j:
-                            if min_result > A[S - j][k] + C[k][j]:
-                                min_result = A[S - j][k] + C[k][j]
-                    A[S][j] = min_result
-    
-    result = inf
-    for j in range(2, n+1):
-        if result > A[fullSet, j] + C[j][1]:
-            result = A[fullSet, j] + C[j][1]
-    return result
-
-paths_to_cases = [
+paths = [
     './inputs/1.txt',
     './inputs/2.txt'    
 ]
-
 '''
-paths_to_cases = [
+paths = [
     './inputs/tsp.txt',    
 ]
 '''
 
-cases = []
-for path_to_case in paths_to_cases:    
-    # ------------- Считывание тестового файла --------
-    lines = open(path_to_case).read().splitlines()
-    points_count = int(lines[0])
-    case = test_helper.TestCase(path_to_case, points_count)
-    for line in lines[1:]:
-        if (line.strip() == ''):
-            continue
-        if(line.find('Answer') != -1):
-            case.parse_answer(line)
-            break
-        case.set_point(line)        
-    cases.append(case)
+def setsAsymDiff(set1, set2): # NOTE возвращается сет, а не список
+    return set1 ^ set2
 
-# ------------- Проверка результатов ------------
-for case in cases:
-    #print(case)
-    print('path_to_case: {},\nexpected_answer: {}'.format(case.path_to_case, case.answer))    
-    actual_answer = tsp(case.points)
-    print('actual_answer: {}'.format(actual_answer) )  
+def tsp(points):    
+    IntermediatePointsToDestination = SetIndexedByTuple()
+    n = len(points)
+    initBaseCase(IntermediatePointsToDestination, n)
+    
+    #graphviz = GraphvizOutput()
+    #graphviz.output_file = './output.png'
+    #with PyCallGraph(output=GraphvizOutput()):  
+    for m in range(1, n+1):
+        print("m: {}".format(m))     
+        collections = generateTuplesWithoutReturnInAscending(0, n - 1, m)
+        for S in collections:   
+            for j in S:
+                if j != 0:
+                    min_result = inf
+                    for k in S:
+                        if k != j:
+                            S_without_j = setsAsymDiff(set(S), set([j]))
+                            toDestinationDistances = IntermediatePointsToDestination.Get(tuple(S_without_j))                              
+                            if k in toDestinationDistances:                                
+                                if min_result > toDestinationDistances[k] + C[k][j]:
+                                    min_result = toDestinationDistances[k] + C[k][j]                        
+                    IntermediatePointsToDestination.Get(S)[j] = min_result #A TIP: keep only the last row/column of subproblems, etc.
+    result = inf
+    fullSet = tuple()
+    for i in range(n):
+        fullSet = fullSet + (i,)
+    
+    for j in range(1, n):              
+        allPointsToDestination = IntermediatePointsToDestination.Get(fullSet)
+        
+        allPointsToJ = allPointsToDestination[j] 
+        if allPointsToJ + C[j][0] < result: # NOTE при индексации в сете порядок не важен
+            result = IntermediatePointsToDestination.Get(fullSet)[j] + C[j][0]
+    return result
+
+def initBaseCase(IntermediatePointsToDestination, n):
+    for i in range(n):
+        if i == 0:
+            IntermediatePointsToDestination.Get(tuple([0]))[i] = 0            
+        else:
+            IntermediatePointsToDestination.Get(tuple([0]))[i] = inf
+
+cases = test_helper.readCases(paths)
+
+for case in cases:    
+    print('path: {},\nexpected_answer: {}'.format(case.path, case.answer))
+    C = calcDistances(case.points)    
+    print(C)
+    print('----------')    
+
+    actualAnswer = tsp(case.points)
+    print('actualAnswer: {}'.format(actualAnswer) )  
     if(case.answer is not None):
-        if case.is_passed(actual_answer):
+        if case.isPassed(actualAnswer):
             print('Test case SUCCEEDED!')
         else:
             print('Test case FAILED!')
