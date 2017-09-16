4332a749e5ae9d00b74234ae4332a66eea510f63
a_sitnikov <a_sitnikov@head-point.ru>
2017-09-16 18:06:43 +0100

    distances

--- travelling-salesman-problem/tsp.py
+++ travelling-salesman-problem/tsp.py
@@ -10,24 +10,24 @@ from collection_generator import generateTuplesWithoutReturnInAscending
 from pycallgraph import PyCallGraph
 from pycallgraph.output import GraphvizOutput
 
-global IntermediatePointsToDestination
-global C
+global pointsToDst
+# global distances
 
 def calcDistance(x1, y1, x2, y2): return math.sqrt(math.pow(x1-x2, 2) + math.pow(y1-y2, 2))
 
 def calcDistances(points):    
     length = len(points)
-    C = [[0 for j in range(length)] for i in range(length)]
+    result = [[0 for j in range(length)] for i in range(length)]
 
     for i in range(length):
         for j in range(i+1, length):
             dist = calcDistance(points[i][0], points[i][1], points[j][0], points[j][1])
             if(dist != 0):
-                C[i][j] = dist
-                C[j][i] = dist
+                result[i][j] = dist
+                result[j][i] = dist
             else:
                 del points[j]
-    return C 
+    return result
 
 inf = float('inf')
 
@@ -40,15 +40,16 @@ inputCases = [
 def setsAsymDiff(set1, set2): # NOTE возвращается сет, а не список
     return set1 ^ set2
 
-def tsp(points):    
-    IntermediatePointsToDestination = SetIndexedByTuple()
+def tsp(path, points, distances):    
+    pointsToDst = SetIndexedByTuple()
     n = len(points)
-    initBaseCase(IntermediatePointsToDestination, n)
+    initBaseCase(pointsToDst, n)
 
-    #graphviz = GraphvizOutput()
-    #graphviz.output_file = './output.png'
-    #with PyCallGraph(output=graphviz):  
-    prev_IntermediatePointsToDestination = SetIndexedByTuple.Clone(IntermediatePointsToDestination)
+    # graphviz = GraphvizOutput()
+    # graphviz.output_file = f'./{path}.png'
+    # with PyCallGraph(output=graphviz):  
+    prev_pointsToDst = SetIndexedByTuple.Clone(pointsToDst)
+    # for m in range(1, 4):
     for m in range(1, n+1):
         print(f"m: {m}")
         tsBefore = time.time()     
@@ -59,7 +60,7 @@ def tsp(points):
         loggingInterval = 30
         tsStartPoint = time.time()
         for S in collections:   # LATEST IDEA : THIS BLOCK PROCESSED FOR A LONG TIME            
-            intermediatePoints = IntermediatePointsToDestination.Get(S)
+            intermediatePoints = pointsToDst.Get(S)
             for j in S:
                 if j != 0:
                     min_result = inf                    
@@ -67,19 +68,20 @@ def tsp(points):
                         if k != j:        
                             # idea to check if memory is enough (for that sake we omit long calculations)                                            
                             S_without_j = setsAsymDiff(set(S), set([j]))                            
-                            toDestinationDistances = prev_IntermediatePointsToDestination.Get(tuple(S_without_j))                               
+                            toDestinationDistances = prev_pointsToDst.Get(tuple(S_without_j))                               
                             #if k in toDestinationDistances:
-                            if min_result > toDestinationDistances[k] + C[k][j]:
-                                min_result = toDestinationDistances[k] + C[k][j]                                                    
+                            if min_result > toDestinationDistances[k] + distances[k][j]:
+                                min_result = toDestinationDistances[k] + distances[k][j]                                                    
                              
                     intermediatePoints[j] = min_result # I used A TIP: keep only the last row/column of subproblems, etc.
             tsAfterIteration = time.time()
             if tsAfterIteration - tsStartPoint > loggingInterval:                
                 print(tsAfterIteration - tsBefore)
                 tsStartPoint = tsAfterIteration
-        prev_IntermediatePointsToDestination.Clear()
-        prev_IntermediatePointsToDestination = SetIndexedByTuple.Clone(IntermediatePointsToDestination)
-        IntermediatePointsToDestination.Clear()
+        prev_pointsToDst.Clear()
+        prev_pointsToDst = pointsToDst
+        prev_pointsToDst = SetIndexedByTuple.Clone(pointsToDst)
+        pointsToDst.Clear()
         tsAfter = time.time()
         print(tsAfter - tsBefore)
     result = inf
@@ -87,30 +89,30 @@ def tsp(points):
     for i in range(n):
         fullSet = fullSet + (i,)
     
-    allPointsToDestination = prev_IntermediatePointsToDestination.Get(fullSet)
+    allPointsToDestination = prev_pointsToDst.Get(fullSet)
     print(allPointsToDestination)
     for j in range(1, n):                              
         allPointsToJ = allPointsToDestination[j] 
-        if allPointsToJ + C[j][0] < result: # NOTE при индексации в сете порядок не важен
-            result = allPointsToJ + C[j][0]
+        if allPointsToJ + distances[j][0] < result: # NOTE при индексации в сете порядок не важен
+            result = allPointsToJ + distances[j][0]
     return result
 
-def initBaseCase(IntermediatePointsToDestination, n):
+def initBaseCase(pointsToDst, n):
     for i in range(n):
         if i == 0:
-            IntermediatePointsToDestination.Get(tuple([0]))[i] = 0            
+            pointsToDst.Get(tuple([0]))[i] = 0            
         else:
-            IntermediatePointsToDestination.Get(tuple([0]))[i] = inf
+            pointsToDst.Get(tuple([0]))[i] = inf
 
 print('!')
 cases = readCases(inputCases)
 
 for case in cases:    
     print(f'path: {case.path},\nexpected_answer: {case.answer}')
-    C = calcDistances(case.points)        
+    distances = calcDistances(case.points)        
     print('----------')    
 
-    actualAnswer = tsp(case.points)
+    actualAnswer = tsp(case.path, case.points, distances)
     print(f'actualAnswer: {actualAnswer}')  
     if(case.answer is not None):
         if case.roundAnswer:
